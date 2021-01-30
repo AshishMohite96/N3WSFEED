318f88cf77c255c1928053cdf8d65fe351035b26
Ashish Mohite <ashishmohite1996@gmail.com>
2021-01-30 20:36:52 +0530

    search and favorite UI added

--- app/src/main/java/com/ak/newsfeed/ui/favorite/NewsFavFragment.kt
+++ app/src/main/java/com/ak/newsfeed/ui/favorite/NewsFavFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 
 import com.ak.newsfeed.R
 import com.ak.newsfeed.databinding.FragmentNewsFavBinding
+import com.ak.newsfeed.ui.home.NewsListAdapter
+import com.ak.newsfeed.utils.DiffUtilNewsItemCallback
+import com.ak.newsfeed.utils.FakeDataSource
 
 /**
  * A simple [Fragment] subclass.
@@ -15,10 +19,20 @@ import com.ak.newsfeed.databinding.FragmentNewsFavBinding
 class NewsFavFragment : Fragment(R.layout.fragment_news_fav) {
 
     private lateinit var binding: FragmentNewsFavBinding
+    private var adapter: NewsListAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNewsFavBinding.bind(view)
+
+        binding.apply {
+            rvFav.setHasFixedSize(true)
+            rvFav.layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter = NewsListAdapter(DiffUtilNewsItemCallback())
+            rvFav.adapter = adapter
+            val fakeDataSource = FakeDataSource()
+            adapter?.submitList(fakeDataSource.getFakeStaticListNews())
+        }
     }
 
 }
--- app/src/main/java/com/ak/newsfeed/ui/search/SearchFragment.kt
+++ app/src/main/java/com/ak/newsfeed/ui/search/SearchFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 
 import com.ak.newsfeed.R
 import com.ak.newsfeed.databinding.FragmentSearchBinding
+import com.ak.newsfeed.ui.home.NewsListAdapter
+import com.ak.newsfeed.utils.DiffUtilNewsItemCallback
+import com.ak.newsfeed.utils.FakeDataSource
 
 /**
  * A simple [Fragment] subclass.
@@ -15,10 +19,20 @@ import com.ak.newsfeed.databinding.FragmentSearchBinding
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private lateinit var binding: FragmentSearchBinding
+    private var adapter: NewsListAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSearchBinding.bind(view)
+
+        binding.apply {
+            searchRv.setHasFixedSize(true)
+            searchRv.layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter = NewsListAdapter(DiffUtilNewsItemCallback())
+            searchRv.adapter = adapter
+            val fakeDataSource = FakeDataSource()
+            adapter?.submitList(fakeDataSource.getFakeListNews())
+        }
     }
 
 }
